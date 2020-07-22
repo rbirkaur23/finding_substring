1abcbee2f30ae3127ede709a896935fdf2e10623
rbirkaur23 <64214931+rbirkaur23@users.noreply.github.com>
2020-07-22 22:35:15 +0530

    substringfind.py created
    finds if substring is present in the main string or not.

---
+++ substringfind.py
@@ -0,0 +1,6 @@
+str=input("Enter string: ")
+str2=input("Enter sub string: ")
+if str2 in str:
+  print("Sub string is present in the main string")
+else:
+  print("Sub string is not found in the main string")
